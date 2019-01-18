9c0fc42b1692d6bcb4b8c36c07e917f5bf3b29bd
Felix Moerman <felix.moerman@hotmail.com>
2019-01-18 09:54:14 +0100

    Fixed problem with alpha assignment

--- R/SummarizePosteriors.R
+++ R/SummarizePosteriors.R
@@ -11,15 +11,15 @@ SummarizePosteriors <- function(fulloutput){
   #1) Summarize the posterior data by calculating mean and standard deviations of all the parameters (on log scale)
   {
     sumoutput <- select(fulloutput, ident)
-    sumoutput <- mutate(sumoutput, logr0.mean =NA, logr0.sd=NA, logK.mean=NA, logK.sd=NA, logalfa.mean=NA, logalfa.sd=NA, logd.mean=NA, logd.sd=NA)
+    sumoutput <- mutate(sumoutput, logr0.mean =NA, logr0.sd=NA, logK.mean=NA, logK.sd=NA, logalpha.mean=NA, logalpha.sd=NA, logd.mean=NA, logd.sd=NA)
     for (i in sumoutput$ident){
       temp <- filter(fulloutput, ident==i)
       sumoutput[which(sumoutput$ident==i), ]$logr0.mean <- mean(log(unlist(temp$all_r0_dist)))
       sumoutput[which(sumoutput$ident==i), ]$logr0.sd <- sd(log(unlist(temp$all_r0_dist)))
       sumoutput[which(sumoutput$ident==i), ]$logK.mean <- mean(log(unlist(temp$all_K_dist)))
       sumoutput[which(sumoutput$ident==i), ]$logK.sd <- sd(log(unlist(temp$all_K_dist)))
-      sumoutput[which(sumoutput$ident==i), ]$logalfa.mean <- mean(log(unlist(temp$all_alfa_dist)))
-      sumoutput[which(sumoutput$ident==i), ]$logalfa.sd <- sd(log(unlist(temp$all_alfa_dist)))
+      sumoutput[which(sumoutput$ident==i), ]$logalpha.mean <- mean(log(unlist(temp$all_alpha_dist)))
+      sumoutput[which(sumoutput$ident==i), ]$logalpha.sd <- sd(log(unlist(temp$all_alpha_dist)))
       sumoutput[which(sumoutput$ident==i), ]$logd.mean <- mean(log(unlist(temp$all_d_dist)))
       sumoutput[which(sumoutput$ident==i), ]$logd.sd <- sd(log(unlist(temp$all_d_dist)))
     }
